3511a43e06065f0dd0ed03c495cc7b860c0bc819
Anna <an8448@mail.ru>
2022-12-20 20:04:28 +0500

    Задача к 7 семинару

---
+++ Phonebook/view.py
@@ -0,0 +1,14 @@
+import csv
+
+def choose_mode():
+    return input('Введите дейсвие (1-добавление, 2-поиск): ')
+
+def new_contact():
+    name = input('Введите имя: ')
+    phone_num = input('Введите номер телефона: ')
+    return f'{name} || {phone_num}'
+
+def show_found(result):
+    print ('Результат поиска: ')
+    for i in result:
+        print(i)
